5c9fb09f5d606a8aed07c7a7747969a0829023cc
avinashpitti <avinash87129@gmail.com>
2025-12-19 08:35:56 +0530

    added some operations into operators folder

---
+++ operators/Repetition.py
@@ -0,0 +1,9 @@
+qty=2
+price=399
+brand="Indian Terrain "
+color="brown"
+rating=3.8
+print(price*qty) # If both are int it multiplies
+print(brand*qty) # If one is int and another is str it does repetition.
+# print(brand*color) # can't multiply sequence(str) by non integer
+
