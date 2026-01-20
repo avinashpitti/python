ea6c7d18616a63c7103cb391079642c03f2eb03e
avinashpitti <avinash87129@gmail.com>
2026-01-20 20:01:33 +0530

    add basics.py file in file handling

---
+++ file_handling/basics.py
@@ -0,0 +1,5 @@
+f=open('data.txt','r')
+print(f.read())
+f.close()
+
+
--- functions/function_classes/decorators.py
+++ functions/function_classes/decorators.py
@@ -58,5 +58,28 @@ print(orders("RG",False))
 print(profile("RG",False))
 
 
+#smart division decorator
+
+def smart_div(func):
+    def inner(a, b):
+        if b == 0:
+            print("cannot divide by zero")
+        else:
+            return func(a, b)
+    return inner
+
+
+@smart_div
+def calc(a, b):
+    print(a / b)
+
+
+calc(10, 5)
+calc(10, 0)
+
+
+
+
+
 
 
