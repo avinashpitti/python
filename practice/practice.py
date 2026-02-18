ee5392049c3f204d4245ac5bfb39c96f571d546f
avinashpitti <avinash87129@gmail.com>
2026-02-18 18:41:48 +0530

    practice sets

--- practice/practice.py
+++ practice/practice.py
@@ -1,13 +1,37 @@
-t=(10,20,30)
-print(type(t))
-print(t[0])
-print(t[0:2])
+s={}
+print(type(set))
 
+se=set()
+print(type(se))
 
-x=(1,2,[3,4,5])
-x[2][2]=100
-print(x)
+a={1,2,3,2,2,1,6}
+print(a)
+a.add(7)
+print(a)
+a.remove(3) # throws error if not present
+print(a)
 
-y=(10,20,(30,40))
-y[2][2]=444
-print(y)
+a.discard(5) # Doesn't show error if not present
+print(a)
+
+a.pop()
+print(a) # since set is unordered pop removes random element
+
+a.clear()
+print(a)
+
+x={1,2,3,4}
+y={3,4,5,6,7}
+
+print(x | y) # union
+print(x & y) # intersection
+print(x - y)
+print(x ^ y) # symmetric difference
+
+z={1,2,3}
+z.update([3,4,5])
+print(z)
+
+v={1,2,3}
+v.update([4])
+print(v)
