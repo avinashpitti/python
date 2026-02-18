57a4c7982f15db3e59ab1c2e025e8ba02c1ff505
avinashpitti <avinash87129@gmail.com>
2026-02-18 21:32:05 +0530

    dictionary practice

--- practice/practice.py
+++ practice/practice.py
@@ -1,67 +1,96 @@
 student={
     'name':'Avinash',
-    'age' :22,
-    'course':'python',
-    'gender':'Male'
+    'age':22,
+    'address':{
+        'city':'Bangalore',
+        'state':'Karnataka',
+        'zipcode':500037
+    }
 }
 
+print(student)
 print(type(student))
-print(student['name'])
-print(student['age'])
-print(student)
+print(student['address'])
+print(student['address']['city'])
+print(student['address']['zipcode'])
 
-for key in student:
-    print(key)
+student['address']['city']='Mysore'
+print(student['address'])
 
-for key,value in student.items():
-    print(key,value)
-
-print(student.keys())
-print(student.values())
-
-del student['age']
-print(student)
-
-student['city']='Bangalore'
-print(student)
-
-student['age']='65'
-print(student)
-
-# print(student('xyz')) # keyerror
-print(student.get('xyz')) # None
-
-print('Here we go')
-
+print("********************")
 
 for key,value in student.items():
     print(key,value)
 
-print(student.keys())
-print(student.values())
-print(student.items())
+for key,value in student['address'].items():
+    print(key,value)
 
 
-for key in student:
-    print(key)
+employees = {
+    101: {"name": "Rahul", "age": 30},
+    102: {"name": "Sonia", "age": 28},
+    103: {"name": "Priya", "age": 25}
+}
 
-print("**********************")
-for value in student:
-    print(value)
+print(employees[102]['name'])
+employees[103]["age"]=26
+print(employees[103]["age"])
 
-print("**********************")
+employees[104]={"name":"modi","age":45}
 
-for value in student.values():
-    print(value)
+print(employees[104])
+print(employees)
 
-print("**********************")
+# printing the employees who is greater than 28
+
+for emp in employees.values():
+    if emp["age"] > 28 :
+        print(emp['name'])
 
 
-for value in student.items() :
-    print(value)
+for emp in employees.values():
+    if emp["name"].lower().startswith("p"):
+        print(emp)
 
-print("**********************")
+nums=[1,2,3,4]
+squares={n*n for n in nums}
+print(squares)
 
-d={'x':10}
-d.update({'x':50,'y':100})
-print(d)
+numbers=[1,2,3,4,5,6]
+sqrs={n:n*n for n in numbers}
+print(sqrs)
+
+even_sqrs={n:n*n for n in numbers if n % 2==0}
+print(even_sqrs)
+
+students={
+    'Rahul':85,
+    "Sonia":92,
+    'priyanka':78,
+    'Modi':87
+}
+
+# students with marks >80
+
+top_students={name:marks for name ,marks in students.items() if marks > 80}
+print(top_students)
+
+updating={name:marks+5 for name,marks in students.items()}
+print(updating)
+
+employees = {
+    101: {"name": "Rahul", "age": 30},
+    102: {"name": "Sonia", "age": 28},
+    103: {"name": "Priya", "age": 25}
+}
+
+# for emp in employees.values() :
+#     if emp["age"]>26:
+#         print(emp)
+
+filtered={
+    emp_id:emp
+    for emp_id,emp in employees.items()
+    if emp['age']>25
+}
+print(filtered)
