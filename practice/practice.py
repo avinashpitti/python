91e70fe01c54e9448261b7b87acff7a1fcd4c6cb
avinashpitti <avinash87129@gmail.com>
2026-02-19 17:01:32 +0530

    datatypes practice

--- practice/practice.py
+++ practice/practice.py
@@ -1,30 +1,40 @@
 #1
-x = 10
-y = 3
-print(x / y) # 3.33 (division)
-print(x // y) # 3 (floor division)
-print(type(x / y)) # It divides as a normal division so float
-print(type(x // y)) # floor division eliminates decimal part. So it becomes int
+a = "5"
+b = 3
+print(a * b) # "555"(here b is converted into str)
+print(int(a) + b) # 8(here we are converting a into int)
+# print(a + b) # we can't concatenate str+int,so typerror
 
 #2
-s = "Hello, World!"
-print(s[7:12]) # World(In slicing start value is included,end is not)
-print(s[-6:-1]) # World(Negative indexing starts from -1, not 0)
-print(s[::-1]) # !dlroW ,olleH(It reverses the string)
-print(s[-1:-6]) # Expalin this to me in detail
+a = [1, 2, 3]
+b = a # b=a means both are referring to same object memory
+b.append(4) # so appending one equal to appending both
+print(a) # [1,2,3,4]
+print(b) # [1,2,3,4]
+
+c = a.copy() # Here c is a copy of a (it will have different memory)
+c.append(5) # The append value is only added to c
+print(a) # [1,2,3,4]
+print(c) # [1,2,3,4,5]
+# Note : Explain this problem clearly and also about
+# aliasing vs copy vs shallow copy vs deep copy and hashable vs unhashable with examples
 
 #3
-nums = [1, 2, 3, 4, 5]
-nums.append(6)# [1,2,3,4,5,6](append adds at the end)
-nums.insert(0, 0)# [0,1,2,3,4,5,6](inserting value 0 at index 0)
-print(nums[::2])#[0,2,4,6](step value is 2)
+s1 = {1, 2, 3, 4}
+s2 = {3, 4, 5, 6}
+print(s1 & s2)# {3,4}(and returns common values in both the sets)
+print(s1 | s2)# {1,2,3,4,5,6}(or returns all the values in sets by removing duplicates)
+print(s1 - s2)# {1,2}(it prints values in s1 which are not in s2)
+print(s1 ^ s2)#{1,2,5,6}(It's opposite to and)
 
 #4
-d = {"a": 1, "b": 2, "c": 3}
-d["d"] = 4 # since key(d) is not exists, it creates one
-del d["a"] # It deletes the key a
-print(list(d.keys())) # {b,c,d}
-print(d.get("a", "not found")) # we have used safe method get(so no error)
+t = (1, 2, 3, 2, 1)# tuple allows heterogeneous elements
+print(t.count(2))# count(2) is 2
+print(t.index(3))# index of 3 is 2
+# t[0] = 10 # tuple values are immutable (typeerror)
 
-
-
+#5
+d1 = {"a": 1, "b": 2}
+d2 = {"b": 99, "c": 3}
+merged = {**d1, **d2}
+print(merged) 
