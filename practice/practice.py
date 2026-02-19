fecca2b3b6a6e6d8d7a3ab7af05925fe99bd3681
avinashpitti <avinash87129@gmail.com>
2026-02-19 12:32:29 +0530

    data structure practice

--- practice/practice.py
+++ practice/practice.py
@@ -1,74 +1,30 @@
-# mutable vs immutable
-# mutable : mutable means can be changed after creation
-# If you modify it the memory address remains the same
-# list,set,dict,bytearray are mutable datatypes.
+#1
+x = 10
+y = 3
+print(x / y) # 3.33 (division)
+print(x // y) # 3 (floor division)
+print(type(x / y)) # It divides as a normal division so float
+print(type(x // y)) # floor division eliminates decimal part. So it becomes int
 
-# Immutable : Immutable means can't be changed after creation
-# If you modify it, python creates a new object in memory
-# Remaining all are immutable.
+#2
+s = "Hello, World!"
+print(s[7:12]) # World(In slicing start value is included,end is not)
+print(s[-6:-1]) # World(Negative indexing starts from -1, not 0)
+print(s[::-1]) # !dlroW ,olleH(It reverses the string)
+print(s[-1:-6]) # Expalin this to me in detail
 
+#3
+nums = [1, 2, 3, 4, 5]
+nums.append(6)# [1,2,3,4,5,6](append adds at the end)
+nums.insert(0, 0)# [0,1,2,3,4,5,6](inserting value 0 at index 0)
+print(nums[::2])#[0,2,4,6](step value is 2)
 
-nums=[1,2,3]
-print(id(nums))
-print(nums)
-
-nums.append(4)
-print(id(nums))
-print(nums)
-
-x=10
-print(id(x))
-print(x)
-x+=1
-print(id(x))
-print(x)
-
-name='avinash '
-print(id(name))
-print(id)
-
-name+='avi'
-print(id(name))
-print(name)
+#4
+d = {"a": 1, "b": 2, "c": 3}
+d["d"] = 4 # since key(d) is not exists, it creates one
+del d["a"] # It deletes the key a
+print(list(d.keys())) # {b,c,d}
+print(d.get("a", "not found")) # we have used safe method get(so no error)
 
 
-t=(1,2,[3,4,5])
-print(id(t))
-print(t)
-t[2].append(9)
-print(id(t))
-print(t)
 
-a=[1,2]
-b=a
-a=[5,6]
-print(a)
-print(b)
-
-a=[1,2]
-print(id(a))
-b=a
-print(id(b))
-c=a.copy()
-print(id(c))
-a.append(3)
-print(id(a))
-print(a)
-print(b)
-print(c)
-
-import copy
-
-a = [[1, 2], [3, 4]]
-print(id(a))
-b = copy.deepcopy(a)
-print(id(b))
-
-a[0].append(99)
-print(id(a))
-
-print(a)
-print(b)
-
-d = {(1,2): "hello"}  # works
-print(d)
