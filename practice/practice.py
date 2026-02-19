3080418ac9330901e0c98ab7909aa54ce997a747
avinashpitti <avinash87129@gmail.com>
2026-02-19 21:56:02 +0530

    practice datatypes

--- practice/practice.py
+++ practice/practice.py
@@ -1,21 +1,87 @@
-s={1,2,3}
-s.add(6)
-print(s)
-s.discard(5) # discard doesn't show error, if the value doesn't exist 
-print(s)
-# s.remove(5) # remove shows error if the value doesn't exist
+student={
+    'name':'Avinash',
+    'age':20,
+    'grade':'A'
+}
 
-s.update([7,9,45,67])
-print(s)
+print(student['age'])
+print(student.get('age'))
 
-s.remove(9)
-print(s)
+student['phone']=9898989898
+print(student)
 
-a={1,2,3,4}
-b={3,4,5,6}
-print(a & b) #Intersection
-print(a | b) # union
-print(a-b) # difference
-print(a ^ b)# symmetric difference
+student['age']=23
 
 
+print(student.items())
+print(student.keys())
+print(student.values())
+
+for key in student:
+    print(key)
+
+for value in student.values():
+    print(value)
+
+
+print(student.get('age'))
+
+#1
+fruits = ["apple", "banana", "cherry"]
+fruits.append('orange')
+fruits.remove('banana')
+# fruits.pop(1)
+print(fruits) # ['apple','cherry','orange']
+
+#2
+t = (1, 2, 3, 2, 1)
+print(t.count(2)) #2
+print(t.index(3)) #2
+
+#3
+a = {1, 2, 3, 4}
+b = {3, 4, 5, 6}
+print(a & b) # {3,4}
+print(a | b) # {1,2,3,4,5,6}
+print(a - b) # {1,2}
+
+#4
+d = {"x": 10, "y": 20, "z": 30}
+d["x"] = 99
+d.pop("z")
+print(d) # {'x :99,'y':20}
+
+#5.
+fruits = ["apple", "banana", "cherry"]
+print(fruits[1:]) # ['banana','cherry']
+print(fruits[:2]) # ['apple','banana']
+print(fruits[-1]) # ['cherry']
+
+
+#1
+d = {"a": 1, "b": 2, "c": 3}
+for key in d:
+    print(key, d[key])
+
+#2
+lst = [1, 2, 3, 4, 5]
+print(lst[::2])# [1,3,5]
+print(lst[::-1])# [5,4,3,2,1]
+
+#3
+s = {1, 2, 3}
+s.add(2) # Already exists (Duplicates are not allowed)
+s.add(4) # It is added
+print(s) # {1,2,3,4}
+
+#4
+t = (1, 2, 3)
+lst = list(t) # [1,2,3]
+lst.append(4) # [1,2,3,4]
+t2 = tuple(lst) 
+print(t2)# (1,2,3,4)
+
+#5
+d = {"name": "Alice", "age": 20}
+print("name" in d) # 'Alice'
+print("Alice" in d) # 
