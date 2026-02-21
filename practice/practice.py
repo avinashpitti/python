165dec3d0ab9ab376698605f2f470b7f026103dc
avinashpitti <avinash87129@gmail.com>
2026-02-21 21:48:00 +0530

    printing prime numbers from 1 to n

--- practice/practice.py
+++ practice/practice.py
@@ -1,11 +1,15 @@
-# FizzBuzz
+# Find all prime numbers upto n
 
-for i in range(20):
-    if i % 3 ==0 and i % 5==0:
-        print('FizzBuzz')
-    elif i%3==0:
-        print('Fizz')
-    elif i%5==0:
-        print('Buzz')
-    else:
-        print(i)
+n=int(input('Enter a number : '))
+
+
+for num in range(2, n+1):
+    is_prime = True
+    
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            is_prime = False
+            break
+    
+    if is_prime:
+        print(num, end=" ")
